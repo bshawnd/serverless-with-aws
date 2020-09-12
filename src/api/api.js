2b162a5738918c8016c90ad23e896323da28debe
Shawn Blanchard <shawn_blanchard@schumachergroup.com>
2020-09-12 16:57:59 -0500

    update dependencies

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+module.exports.handler = (evt, ctx, done) => {
+  done(null, {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ message: 'Helloooooo' })
+  })
+}
